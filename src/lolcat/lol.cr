06015c0513c9a3f6372a716992e67a7b59e29c00
kojix2 <2xijok@gmail.com>
2024-11-26 15:27:22 +0900

    Enhance real-time processing

--- src/lolcat/lol.cr
+++ src/lolcat/lol.cr
@@ -3,7 +3,8 @@ require "./options"
 
 module Lolcat
   module Lol
-    ANSI_ESCAPE = /((?:\e(?:[ -\/]+.|[\]PX^_][^\a\e]*|\[[0-?]*.|.))*)(.?)/m
+    ANSI_ESCAPE       = /((?:\e(?:[ -\/]+.|[\]PX^_][^\a\e]*|\[[0-?]*.|.))*)(.?)/m
+    INCOMPLETE_ESCAPE = /\e(?:[ -\/]*|[\]PX^_][^\a\e]*|\[[0-?]*)$/
 
     def lol_cat(input : (IO | String), options : Options)
       if options.force?
@@ -29,18 +30,39 @@ module Lolcat
     end
 
     def lol(input : IO, options : Options, &)
-      input.each_line(chomp: false).with_index do |line, index|
-        if line =~ /\n$/
-          yield "#{rainbow_line(line.delete_at(-1), index, options)}\n"
-        else
-          yield rainbow_line(line, index, options)
+      buffer = IO::Memory.new
+      chunk = Bytes.new(4096)
+      line_number = 0
+      bytes_read = 0
+      # FIXME: tab width is 1 for now
+      cursor_position = 0
+
+      while (bytes_read = input.read(chunk)) > 0
+        buffer.write(chunk[0, bytes_read])
+        buffer_content = buffer.to_s
+
+        if buffer_content =~ INCOMPLETE_ESCAPE
+          next
         end
+
+        buffer_content.each_line(chomp: false) do |line|
+          if line =~ /\n$/
+            yield "#{rainbow_line(line.delete_at(-1), line_number, cursor_position, options)}\n"
+            line_number += 1
+            cursor_position = 0
+          else
+            yield rainbow_line(line, line_number, cursor_position, options)
+            cursor_position += line.size # tab width is 1
+          end
+        end
+
+        buffer.clear
       end
     end
 
-    def rainbow_line(line : String, index : Int32, options : Options) : String
+    def rainbow_line(line : String, index : Int32, pos : Int32, options : Options) : String
       colored_line = String.build do |str|
-        char_position = 0 # Track the visible character position
+        char_position = pos # Track the visible character position
 
         line.scan(ANSI_ESCAPE) do |match|
           escape_sequence = match[1]? # Match group 1: ANSI escape codes
