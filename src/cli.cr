404b7de777a2c87a5fd3ce94e3a5f52f64298542
kojix2 <2xijok@gmail.com>
2024-12-12 21:10:57 +0900

    Handle terminal reset on errors in CLI

--- src/cli.cr
+++ src/cli.cr
@@ -48,6 +48,11 @@ module Lolcat
     end
 
     private def handle_error(ex : Exception)
+      if STDOUT.tty? || options.force?
+        # If the function lol#lol_cat passes through rescue, it is a double call.
+        # However, errors that do not pass lol_cat must also be handled.
+        print "#{Lol::RESET_ATTRIBUTES}#{Lol::SHOW_CURSOR}#{Lol::RESET_TERMINAL_MODES}"
+      end
       STDERR.puts "\n[lolcat] ERROR: #{ex.class} #{ex.message}"
       STDERR.puts "\n#{ex.backtrace.join("\n")}" if CLI.debug?
       exit(1)
