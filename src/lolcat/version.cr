5ad1f2aa16573a213227185d72f876b3a48a2d6e
kojix2 <2xijok@gmail.com>
2025-12-28 16:55:37 +0900

    Refactor version and repository retrieval to use macro

--- src/lolcat/version.cr
+++ src/lolcat/version.cr
@@ -1,4 +1,11 @@
 module Lolcat
-  VERSION  = {{ `shards version #{__DIR__}`.chomp.stringify }}
-  REPOSITORY = {{ `crystal eval 'require "yaml"; File.open("#{__DIR__}/../../shard.yml"){|f| puts YAML.parse(f)["repository"]}'`.chomp.stringify }}
+  macro shard_value(key)
+    {% shard_yml = read_file("#{__DIR__}/../../shard.yml") %}
+    {% lines = shard_yml.lines.select(&.starts_with?(key + ":")) %}
+    {% parts = lines[0].split(":") %}
+    {{ parts[1..-1].join(":").strip.gsub(/^["']|["']$/, "") }}
+  end
+
+  VERSION    = shard_value("version")
+  REPOSITORY = shard_value("repository")
 end
